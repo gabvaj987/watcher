fc1975f5b61c145a1f03c38f42fa481c537fd878
vajdag314 <vajdag314@gmail.com>
2016-05-05 22:16:26 +0200

    initial commit with example get request

---
+++ src/main/java/com/epam/hasznaltauto/HttpGetIssuer.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.epam.hasznaltauto;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+/**
+ * @author Gábor
+ *
+ */
+public class HttpGetIssuer {
+	private final String USER_AGENT = "Chrome/50.0.2661.94";
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		HttpGetIssuer httpGetIssuer = new HttpGetIssuer();
+		httpGetIssuer.sendGet();
+	}
+
+	// HTTP GET request
+	private void sendGet() {
+
+		String url = "http://www.google.hu/";
+
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+		try (CloseableHttpClient httpClient = httpClientBuilder.build()) {
+			HttpGet request = new HttpGet(url);
+
+			// add request header
+			request.addHeader("User-Agent", USER_AGENT);
+
+			HttpResponse response = httpClient.execute(request);
+
+			System.out.println("\nSending 'GET' request to URL : " + url);
+			System.out.println("Response OK : " + (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
